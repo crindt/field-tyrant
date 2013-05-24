5db6e27a33208ebd99c8a29e86754cfb1f7cb23f
Craig Rindt <crindt@gmail.com>
2013-05-24 09:54:08 -0700

    Add command-line examples

---
+++ push-cal-ex.sh
@@ -0,0 +1,3 @@
+node cal.js -c test --from 2013-05-24 --to 2013-06-17 -s ./public/sched/june-2013-sched.json --omit '(ESL|Encinitas|Rugby)' --overwrite schedule berkS 'CSL Berkich South'
+node cal.js -c test --from 2013-05-24 --to 2013-06-17 -s ./public/sched/june-2013-sched.json --omit '(ESL|Encinitas|Rugby)' --overwrite schedule berkN 'CSL Berkich North'
+node cal.js -c test --from 2013-05-24 --to 2013-06-17 -s ./public/sched/june-2013-sched.json --omit '(ESL|Encinitas|Rugby)' --overwrite schedule ada 'CSL Ada (East and West)'
---
+++ sched-cmd-ex.sh
@@ -0,0 +1,1 @@
+node gen.js -ap  data/june-2013.json --format=json -o public/sched/june-2013-sched.json
