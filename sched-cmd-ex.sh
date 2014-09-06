722411b483ebbf06bb361a0e92ed95d47929253d
Craig Rindt <crindt@gmail.com>
2014-09-05 17:46:58 -0700

    Hertz/Avis swap, Cherwick/Thielman/Levy swap, Mullen micro

--- sched-cmd-ex.sh
+++ sched-cmd-ex.sh
@@ -1,1 +1,2 @@
-node gen.js -ap -i data/fall-2013.json --format=json -o public/sched/fall-2013-sched.json -- --presolve --timeout 5
+node gen.js -ap -d 1830 -i data/fall-2014.json --format=json -o public/sched/fall-2014-sched.json -- --presolve --timeout 10
+node ss.js -s "Cardiff Soccer Fall 2014 Practice Schedule v5" -d fall-2014
